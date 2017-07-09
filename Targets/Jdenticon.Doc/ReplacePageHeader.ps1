647bcd2c86ff7bd8657af08ef4cd838049c445b6
daniel <daniel@mester.se>
2017-07-09 12:48:09 +0200

    Added main site navigation header to documentation.

---
+++ Targets/Jdenticon.Doc/ReplacePageHeader.ps1
@@ -0,0 +1,15 @@
+﻿Param(
+  [string]$htmlPath,
+  [string]$headerPath
+)
+
+$oldHeader = '<div class="pageHeader" id="PageHeader">Jdenticon-net documentation<form id="SearchForm" method="get" action="#" onsubmit="javascript:TransferToSearchPage(); return false;"><input id="SearchTextBox" type="text" maxlength="200" /><button id="SearchButton" type="submit"></button></form></div>';
+$newHeader = Get-Content $headerPath
+
+Get-ChildItem $htmlPath -Filter *.htm | 
+Foreach-Object {
+    $content = Get-Content $_.FullName
+	$content = $content.replace($oldHeader, $newHeader)
+
+	Set-Content -Path $_.FullName -Value $content -Encoding UTF8
+}
