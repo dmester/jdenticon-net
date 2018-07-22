2599d95e097f709ca14fd5c683f95c9c3e3511e9
daniel <daniel@mester.se>
2018-07-22 12:06:41 +0200

    Moved signing key to separate build property.

--- Core/AssemblyInfo/GlobalAssemblyInfo.cs
+++ Core/AssemblyInfo/GlobalAssemblyInfo.cs
@@ -7,7 +7,12 @@ using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 
 [assembly: ComVisible(false)]
+
+#if SIGNED_ASSEMBLIES
 [assembly: InternalsVisibleTo("Jdenticon.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100012ca8562f4f6c8ad35009ec2b704d81c664202e81f65adcaa0f795ffa04005031ef1eb8c649b25ebadff90957da4d09f32372ebdc30b672c68e491d73b8e15d3e17b20b9107f704c8f21294c59eca4d95efb4eff2fd3caf30dd8862726a1920101729bf6db6d3d3b81e0ff8165250fc5108e076926606a407a2138e5b0430d8")]
+#else
+[assembly: InternalsVisibleTo("Jdenticon.Tests")]
+#endif
 
 #endif
 
