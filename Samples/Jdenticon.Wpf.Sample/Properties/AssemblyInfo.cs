4821f1d859df6a1cdf173324c3fc96a0d388f534
dmester <daniel@mester.se>
2021-01-04 18:42:27 +0100

    Added version to WPF sample project to avoid strong naming issue.

--- Samples/Jdenticon.Wpf.Sample/Properties/AssemblyInfo.cs
+++ Samples/Jdenticon.Wpf.Sample/Properties/AssemblyInfo.cs
@@ -2,13 +2,3 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Jdenticon.Wpf.Sample")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Jdenticon.WinForms.Sample")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
